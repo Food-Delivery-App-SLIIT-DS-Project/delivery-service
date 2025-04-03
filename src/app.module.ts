2320779e31a01f21b7215f5e6bb64afdd9b5909d
sajeebro <sajithmadusanka720@gmail.com>
2025-04-03 10:20:40 +0530

    added db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
+import { ConfigModule } from '@nestjs/config';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DeliveryModule } from 'delivery/delivery.module';
 
 @Module({
-  imports: [DeliveryModule],
+  imports: [
+    DeliveryModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/default-db',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
